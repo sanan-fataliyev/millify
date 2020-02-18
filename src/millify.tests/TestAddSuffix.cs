43aa337024ce2840413fb1a48e2efeaae37f616e
Sanan Fataliyev <sanan.fataliyev@gmail.com>
2020-02-19 02:43:50 +0400

    more tests

--- src/millify.tests/TestAddSuffix.cs
+++ src/millify.tests/TestAddSuffix.cs
@@ -3,7 +3,6 @@ namespace Millify.Tests
 {
     public class TestAddSuffix
     {
-
         [Theory]
         // da-də
         [InlineData("da","yol","yolda")]
@@ -15,12 +14,19 @@ namespace Millify.Tests
         [InlineData("la","qayçı","qayçıla")]
         [InlineData("la","yel","yellə")]
         [InlineData("la","balta","baltala")]
-        
+
         // ma-mə
         [InlineData("ma","yaz","yazma")]
         [InlineData("ma","gəz","gəzmə")]
         [InlineData("ma","basdala","basdalama")]
-        public void TestInline(string suffix, string root, string expected)
+
+        // lı
+        [InlineData("lı","savad","savadlı")]
+        [InlineData("lı","bilik","bilikli")]
+        [InlineData("lı","uğur","uğurlu")]
+        [InlineData("lı","güc","güclü")]
+
+        public void TestAll(string suffix, string root, string expected)
         {
             Assert.Equal(expected, root.AddSuffix(suffix));
         }
